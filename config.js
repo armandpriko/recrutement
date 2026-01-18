bbb3de0dea125dbf6e08759160c1f8c0eb5b9015
Dzudzogbe Prince Armand, KOUMI <koumiprince@gmail.com>
2026-01-18 20:25:46 +0100

    Fix CONFIG undefined error by adding config.js script import

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 // Configuration file for API keys and sensitive data
 
 const CONFIG = {
-    GEMINI_API_KEY: "AIzaSyA6M99YxYjdF2r2c-_fYQTnK3J2lS64ORA", 
-    GEMINI_MODEL: "gemini-2.5-flash-preview-09-2025", 
+  
     GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwF13JJkNu8EAJPYtRnMmC-4m6zxUMAN6WO9I_kDimhIgOo2LHaoZCLClb2Nb1Ec8dt/exec" // Your Google Apps Script URL for form submission
 };
