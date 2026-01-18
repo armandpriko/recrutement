c47c547ba7ac690d5ef7599638db2b196de2b2e3
Dzudzogbe Prince Armand, KOUMI <101747125+armandpriko@users.noreply.github.com>
2026-01-18 17:38:59 +0100

    Update GEMINI_MODEL to new version

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 
 const CONFIG = {
     GEMINI_API_KEY: "AIzaSyA6M99YxYjdF2r2c-_fYQTnK3J2lS64ORA", 
-    GEMINI_MODEL: "gemini-1.5-flash", 
+    GEMINI_MODEL: "gemini-2.5-flash-preview-09-2025", 
     GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxScl2_-SuwEGN1vkH4hIcmnCwPIbRMNS6Q6wV0bs9PvxPNtAJAijuQXkXHeYrtxJo/exec" // Your Google Apps Script URL for form submission
 };
