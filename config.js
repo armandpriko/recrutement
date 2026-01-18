4f253df4c8d588b0031421a3abf1fb0d6d75f363
Dzudzogbe Prince Armand, KOUMI <koumiprince@gmail.com>
2026-01-18 20:18:01 +0100

    Remove AI features; add role requirements boxes; replace numero with email; update footer with CTA

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 const CONFIG = {
     GEMINI_API_KEY: "AIzaSyA6M99YxYjdF2r2c-_fYQTnK3J2lS64ORA", 
     GEMINI_MODEL: "gemini-2.5-flash-preview-09-2025", 
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxScl2_-SuwEGN1vkH4hIcmnCwPIbRMNS6Q6wV0bs9PvxPNtAJAijuQXkXHeYrtxJo/exec" // Your Google Apps Script URL for form submission
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwF13JJkNu8EAJPYtRnMmC-4m6zxUMAN6WO9I_kDimhIgOo2LHaoZCLClb2Nb1Ec8dt/exec" // Your Google Apps Script URL for form submission
 };
